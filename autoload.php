96b9b96bfee15f6c5d0d0ba09570ccd11a733e66
paulmajora <paulmajora@gmail.com>
2015-08-18 12:16:35 +0000

    auto bootrap files
    uploaded auto bootrap files

---
+++ autoload.php
@@ -0,0 +1,11 @@
+<?php 
+	
+	require_once('framework/file_io.php');
+	require_once('framework/database.php');
+	require_once('framework/persistentSession.php');
+	require_once('framework/ussdSession.php');
+	require_once('framework/ussdHelper.php');
+	require_once('framework/curler.php');
+
+
+?>
